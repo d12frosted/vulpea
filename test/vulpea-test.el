cef8b9ae983f38ad01aab0af84089d7909433e28
Boris Buliga <boris@d12frosted.io>
2021-01-09 11:53:01 +0200

    (feat) `vulpea-select' function

---
+++ test/vulpea-test.el
@@ -0,0 +1,56 @@
+;;; vulpea-test.el --- Test `vulpea' module -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2020 Boris Buliga
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; Maintainer: Boris Buliga <boris@d12frosted.io>
+;;
+;; Created: 29 Dec 2020
+;;
+;; URL: https://github.com/d12frosted/vulpea
+;;
+;; License: GPLv3
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; Test `vulpea' module.
+;;
+;;; Code:
+
+(require 'vulpea-test-helpers)
+(require 'vulpea)
+
+(describe "vulpea-select"
+  :var ((filter-count 0))
+  (before-all
+    (vulpea-test--init))
+
+  (after-all
+    (vulpea-test--teardown))
+
+  (it "returns all information about note"
+    (spy-on 'org-roam-completion--completing-read
+            :and-return-value "(tag1,tag2) Reference")
+    (expect (vulpea-select "Note")
+            :to-equal
+            (list :path (expand-file-name "reference.org" org-roam-directory)
+                  :title "Reference"
+                  :tags '("tag1" "tag2")
+                  :id "5093fc4e-8c63-4e60-a1da-83fc7ecd5db7")))
+
+  (it "calls FILTER-FN on each item"
+    (spy-on 'org-roam-completion--completing-read
+            :and-return-value "(tag1,tag2) Reference")
+
+    (vulpea-select "Note" nil nil
+                   (lambda (note)
+                     (setq filter-count (+ 1 filter-count))))
+    (expect filter-count :to-equal
+            (caar (org-roam-db-query
+                   [:select (funcall count *)
+                    :from titles])))))
+
+(provide 'vulpea-test)
+;;; vulpea-test.el ends here
--- vulpea.el
+++ vulpea.el
@@ -24,7 +24,57 @@
 ;;
 ;;; Code:
 
+(require 'org-roam)
 (require 'vulpea-meta)
+(require 'vulpea-db)
+
+(defun vulpea-select (prompt &optional initial-prompt completions filter-fn)
+  "Select a note.
+
+PROMPT is a message to present.
+
+INITIAL-PROMPT is the initial title prompt.
+
+COMPLETIONS is a list of completions to be used instead of
+`org-roam--get-title-path-completions`.
+
+FILTER-FN is the name of a function to apply on the candidates
+which takes as its argument an alist of path-completions.  See
+`vulpea--get-title-path-completions' for details."
+  (unless org-roam-mode (org-roam-mode))
+  (let* ((completions (or completions
+                          (vulpea--get-title-path-completions)))
+         (completions (if filter-fn
+                          (seq-filter filter-fn completions)
+                        completions))
+         (title-with-tags (org-roam-completion--completing-read (concat prompt ": ") completions
+                                                                :initial-input initial-prompt))
+         (res (cdr (assoc title-with-tags completions)))
+         (id (vulpea-db-get-id-by-file (plist-get res :path))))
+    (if id
+        (plist-put res :id id)
+      (plist-put res :title title-with-tags))))
+
+(defun vulpea--get-title-path-completions ()
+  "Return an alist for completion.
+
+The car is the displayed title for completion, and the cdr
+contains all the funny stuff."
+  (let* ((rows (org-roam-db-query [:select [files:file titles:title tags:tags files:meta] :from titles
+                                   :left :join tags
+                                   :on (= titles:file tags:file)
+                                   :left :join files
+                                   :on (= titles:file files:file)]))
+         completions)
+    (setq rows (seq-sort-by (lambda (x)
+                              (plist-get (nth 3 x) :mtime))
+                            #'time-less-p
+                            rows))
+    (dolist (row rows completions)
+      (pcase-let ((`(,file-path ,title ,tags) row))
+        (let ((k (org-roam--prepend-tag-string title tags))
+              (v (list :path file-path :title title :tags tags)))
+          (push (cons k v) completions))))))
 
 (provide 'vulpea)
 ;;; vulpea.el ends here
