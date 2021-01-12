520b0b93b4b2f29a7038c251fd8b77da8d014e0a
Boris Buliga <boris@d12frosted.io>
2021-01-12 19:36:09 +0200

    (dev) setup fill-column for readme

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((emacs-lisp-mode . ((fill-column . 70)
                      (indent-tabs-mode . nil)
-                     (elisp-lint-indent-specs . ((vulpea-utils-with-file . 1))))))
+                     (elisp-lint-indent-specs . ((vulpea-utils-with-file . 1)))))
+ (org-mode . ((fill-column . 80))))
