688caf9ae24abbcfb650ed36e8598353d21fd637
Boris Buliga <boris@d12frosted.io>
2021-01-18 09:24:04 +0200

    add license
    As recommended by @riscy in [MELPA][1].
    
      [1]: https://github.com/melpa/melpa/pull/7361#issuecomment-761905870

--- vulpea-meta.el
+++ vulpea-meta.el
@@ -7,6 +7,20 @@
 ;; Package-Version: 0.0.1
 ;; Package-Requires: ((emacs "27.1") (org "9.4.4") (org-roam "1.2.3"))
 ;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
 ;; Created: 29 Dec 2020
 ;;
 ;; URL: https://github.com/d12frosted/vulpea
