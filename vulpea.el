0f7ccb692b8bf354a9f0e3d6102acb02f51ffd72
Boris Buliga <boris@d12frosted.io>
2021-01-29 09:59:50 +0200

    (doc) make it more clear that vulpea is extension for org-roam
    See https://github.com/melpa/melpa/pull/7361#issuecomment-766451986

--- vulpea.el
+++ vulpea.el
@@ -34,7 +34,9 @@
 ;;
 ;;; Commentary:
 ;;
-;; Vulpea is a fox.
+;; Vulpea is a collection of functions for note taking based on `org'
+;; and `org-roam'. In most cases, you should simply load `vulpea'
+;; module to get all available functions.
 ;;
 ;;; Code:
 
