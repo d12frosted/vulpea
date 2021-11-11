ee96f2d7e9d4422b592081d67622b87b2003d33b
Boris Buliga <boris@d12frosted.io>
2021-11-11 18:53:24 +0200

    (feat) actually improve performance of specialized queries
    And again I fall into trap of checking performance of functions with
    empty data set. I used notes that I generated during org-roam v1 and
    they had invalid format of tags, meaning that there were 0 notes in
    the resulting data set, meaning that I didn't really test for notes
    retrieval.
    
    Now that is fixed by the following steps.
    
    1. Test set was generated again with org-roam v2 and it contains 9554
       notes with links, properties and proper tags.
    
    2. To make sure that the data set is not malformed, there is a special
       check that result of benchmarked function is not empty.
    
    3. Last, but the thing that actually fix performance - bulk retrieve
       all matched notes instead of using many calls to
       `vulpea-db-get-by-id`. This means that we have one more HUGE and
       FRIGHTENING SQL query, but seems like performance is much better.
       See updated README file.

--- test/vulpea-perf-test.el
+++ test/vulpea-perf-test.el
@@ -49,147 +49,175 @@
          (_ (url-copy-file vulpea-perf-zip-url zip-file-loc))
          (_ (shell-command (format "mkdir -p %s && unzip -qq %s -d %s" temp-loc zip-file-loc temp-loc)))
          (test-notes-dir (expand-file-name "vulpea-test-notes-master/" temp-loc)))
-    (setq org-roam-directory (expand-file-name "notes-generated/notes/" test-notes-dir)
+    (setq org-roam-directory (expand-file-name "notes/" test-notes-dir)
           org-roam-db-location (expand-file-name "org-roam.db" test-notes-dir))
     ;; fix file path values
     (let ((db (emacsql-sqlite org-roam-db-location)))
       (emacsql db (format "update nodes set file = '\"' || '%s' || replace(file, '\"', '') || '\"'"
                           org-roam-directory)))
     (vulpea-db-autosync-enable)
-    (org-roam-db-autosync-enable)
-    (message "Count of notes: %s" (length (vulpea-db-query)))))
+    (org-roam-db-autosync-enable)))
+
+(defmacro vulpea-benchmark-run (n fn &optional to-str &rest args)
+  "Benchmark FN by running it N times with ARGS.
+
+It also prints some possibly useful information. Result of the
+last FN evaluation is converted to string using provided TO-STR
+function.
+
+Return a list of the last FN evaluation result, the total elapsed
+time for execution, the number of garbage collections that ran,
+and the time taken by garbage collection. See also
+`benchmark-run-compiled'."
+  (declare (indent 1) (debug t))
+  `(let* ((v)
+          (result)
+          (name (symbol-name ,fn)))
+     (message "[%s] begin benchmark with %s invocations"
+              name ,n)
+     (setq result
+           (benchmark-run ,n
+             (setq v (funcall ,fn ,@args))))
+     (message "[%s] benchmark result is %s after %s invocations%s"
+              name result ,n
+              (if ,to-str
+                  (concat " => " (funcall ,to-str v))
+                ""))
+     (cons v result)))
+
+(defun vulpea-query-result-to-str (notes)
+  "Convert NOTES to string."
+  (format "%s notes"
+          (seq-length notes)))
 
 (describe "vulpea performance"
   (before-all
     (vulpea-perf--init))
 
+  (before-each
+    (message "Count of notes: %s"
+             (caar (org-roam-db-query "select count(*) from nodes"))))
+
   (after-all
     (vulpea-test--teardown))
 
   (describe "vulpea-select"
-    :var (duration)
     (it "select without filter is relatively fast on 9500+ notes"
       (spy-on 'completing-read
               :and-return-value "bell-bottom sprue Presently the roots")
-      (setq duration
-            (benchmark-run 10
-              (vulpea-select "Note")))
-      (message "duration = %s" duration)
-      ;; 40 seconds for 10 runs is pretty fast for 9500+ notes
-      (expect (car duration) :to-be-less-than 40))
+      (let* ((runs 10)
+             (bres (vulpea-benchmark-run runs
+                     #'vulpea-select nil "Note")))
+        ;; 60 seconds for 10 runs is pretty fast for 9500+ notes
+        (expect (nth 1 bres) :to-be-less-than 60)))
 
     (it "select with filter is relatively fast on 9500+ notes"
       (spy-on 'completing-read
               :and-return-value "bell-bottom sprue Presently the roots")
-      (setq duration
-            (benchmark-run 10
-              (vulpea-select "Note"
-                             :filter-fn
-                             (lambda (n)
-                               (string-prefix-p
-                                "bell-bottom"
-                                (vulpea-note-title n))))))
-      (message "duration = %s" duration)
-      ;; 40 seconds for 10 runs is pretty fast for 9500+ notes
-      (expect (car duration) :to-be-less-than 40)))
+      (let* ((runs 10)
+             (bres (vulpea-benchmark-run runs
+                     #'vulpea-select nil
+                     "Note"
+                     :filter-fn
+                     (lambda (n)
+                       (string-prefix-p
+                        "bell-bottom"
+                        (vulpea-note-title n))))))
+        ;; 60 seconds for 10 runs is pretty fast for 9500+ notes
+        (expect (nth 1 bres) :to-be-less-than 60))))
 
   (describe "vulpea-db-query-by-tags-some"
     (it "specialized query is faster than generic vulpea-db-query"
       (let* ((runs 10)
-             (tags-all (seq-map
-                        #'car
-                        (org-roam-db-query "select tag from tags group by tag order by count(1) desc")))
-             (tags (append (seq-take tags-all 2)
-                           (last tags-all 2)))
-             (duration1 (benchmark-run runs
-                          (vulpea-db-query-by-tags-some tags)))
-             (duration2 (benchmark-run runs
-                          (vulpea-db-query (lambda (note)
-                                             (let ((note-tags (vulpea-note-tags note)))
-                                               (seq-some
-                                                (lambda (tag)
-                                                  (seq-contains-p note-tags tag))
-                                                tags)))))))
-        (message "runs = %s" runs)
-        (message "duration specialized = %s" duration1)
-        (message "duration generic     = %s" duration2)
-        (expect (car duration1)
-                :to-be-less-than
-                (car duration2)))))
+             (tags '("grape" "region"))
+             (bres1 (vulpea-benchmark-run runs
+                      #'vulpea-db-query-by-tags-some
+                      #'vulpea-query-result-to-str
+                      tags))
+             (bres2 (vulpea-benchmark-run runs
+                      #'vulpea-db-query
+                      #'vulpea-query-result-to-str
+                      (lambda (note)
+                        (let ((note-tags (vulpea-note-tags note)))
+                          (seq-some
+                           (lambda (tag)
+                             (seq-contains-p note-tags tag))
+                           tags))))))
+        (expect (seq-length (nth 0 bres1)) :to-be-greater-than 0)
+        (expect (seq-length (nth 0 bres1)) :to-equal (seq-length (nth 0 bres2)))
+        (expect (nth 1 bres1) :to-be-less-than (nth 1 bres2)))))
 
   (describe "vulpea-db-query-by-tags-every"
     (it "specialized query is faster than generic vulpea-db-query"
       (let* ((runs 10)
-             (tags-all (seq-map
-                        #'car
-                        (org-roam-db-query "select tag from tags group by tag order by count(1) desc")))
-             (tags (append (seq-take tags-all 2)
-                           (last tags-all 2)))
-             (duration1 (benchmark-run runs
-                          (vulpea-db-query-by-tags-every tags)))
-             (duration2 (benchmark-run runs
-                          (vulpea-db-query (lambda (note)
-                                             (let ((note-tags (vulpea-note-tags note)))
-                                               (seq-every-p
-                                                (lambda (tag)
-                                                  (seq-contains-p note-tags tag))
-                                                tags)))))))
-        (message "runs = %s" runs)
-        (message "duration specialized = %s" duration1)
-        (message "duration generic     = %s" duration2)
-        (expect (car duration1)
-                :to-be-less-than
-                (car duration2)))))
+             (tags '("wine" "rating"))
+             (bres1 (vulpea-benchmark-run runs
+                      #'vulpea-db-query-by-tags-every
+                      #'vulpea-query-result-to-str
+                      tags))
+             (bres2 (vulpea-benchmark-run runs
+                      #'vulpea-db-query
+                      #'vulpea-query-result-to-str
+                      (lambda (note)
+                        (let ((note-tags (vulpea-note-tags note)))
+                          (seq-every-p
+                           (lambda (tag)
+                             (seq-contains-p note-tags tag))
+                           tags))))))
+        (expect (seq-length (nth 0 bres1)) :to-be-greater-than 0)
+        (expect (seq-length (nth 0 bres1)) :to-equal (seq-length (nth 0 bres2)))
+        (expect (nth 1 bres1) :to-be-less-than (nth 1 bres2)))))
 
   (describe "vulpea-db-query-by-links-some"
     (it "specialized query is faster than generic vulpea-db-query"
       (let* ((runs 10)
              (links-all (seq-map
                          (lambda (link)
-                           (cons "id" link))
+                           (cons "id" (car link)))
                          (org-roam-db-query "select dest from links where \"type\" = '\"id\"' group by dest order by count(1) desc")))
              (links (append (seq-take links-all 2)
                             (last links-all 2)))
-             (duration1 (benchmark-run runs
-                          (vulpea-db-query-by-links-some links)))
-             (duration2 (benchmark-run runs
-                          (vulpea-db-query (lambda (note)
-                                             (let ((note-links (vulpea-note-links note)))
-                                               (seq-some
-                                                (lambda (link)
-                                                  (seq-contains-p note-links link))
-                                                links)))))))
-        (message "runs = %s" runs)
-        (message "duration specialized = %s" duration1)
-        (message "duration generic     = %s" duration2)
-        (expect (car duration1)
-                :to-be-less-than
-                (car duration2)))))
+             (bres1 (vulpea-benchmark-run runs
+                      #'vulpea-db-query-by-links-some
+                      #'vulpea-query-result-to-str
+                      links))
+             (bres2 (vulpea-benchmark-run runs
+                      #'vulpea-db-query
+                      #'vulpea-query-result-to-str
+                      (lambda (note)
+                        (let ((note-links (vulpea-note-links note)))
+                          (seq-some
+                           (lambda (link)
+                             (seq-contains-p note-links link))
+                           links))))))
+        (expect (seq-length (nth 0 bres1)) :to-be-greater-than 0)
+        (expect (seq-length (nth 0 bres1)) :to-equal (seq-length (nth 0 bres2)))
+        (expect (nth 1 bres1) :to-be-less-than (nth 1 bres2)))))
 
   (describe "vulpea-db-query-by-links-every"
     (it "specialized query is faster than generic vulpea-db-query"
       (let* ((runs 10)
              (links-all (seq-map
                          (lambda (link)
-                           (cons "id" link))
+                           (cons "id" (car link)))
                          (org-roam-db-query "select dest from links where \"type\" = '\"id\"' group by dest order by count(1) desc")))
-             (links (append (seq-take links-all 2)
-                            (last links-all 2)))
-             (duration1 (benchmark-run runs
-                          (vulpea-db-query-by-links-every links)))
-             (duration2 (benchmark-run runs
-                          (vulpea-db-query (lambda (note)
-                                             (let ((note-links (vulpea-note-links note)))
-                                               (seq-every-p
-                                                (lambda (link)
-                                                  (seq-contains-p note-links link))
-                                                links)))))))
-        (message "runs = %s" runs)
-        (message "duration specialized = %s" duration1)
-        (message "duration generic     = %s" duration2)
-        (expect (car duration1)
-                :to-be-less-than
-                (car duration2))))))
+             (links (seq-take links-all 2))
+             (bres1 (vulpea-benchmark-run runs
+                      #'vulpea-db-query-by-links-every
+                      #'vulpea-query-result-to-str
+                      links))
+             (bres2 (vulpea-benchmark-run runs
+                      #'vulpea-db-query
+                      #'vulpea-query-result-to-str
+                      (lambda (note)
+                        (let ((note-links (vulpea-note-links note)))
+                          (seq-every-p
+                           (lambda (link)
+                             (seq-contains-p note-links link))
+                           links))))))
+        (expect (seq-length (nth 0 bres1)) :to-be-greater-than 0)
+        (expect (seq-length (nth 0 bres1)) :to-equal (seq-length (nth 0 bres2)))
+        (expect (nth 1 bres1) :to-be-less-than (nth 1 bres2))))))
 
 (provide 'vulpea-perf-test)
 ;;; vulpea-perf-test.el ends here
