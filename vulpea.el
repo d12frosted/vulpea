38879c859272967674334c6e9208417ea66e5672
Boris Buliga <boris@d12frosted.io>
2025-12-30 09:25:30 +0200

    docs: improve package description and commentary

--- vulpea.el
+++ vulpea.el
@@ -1,4 +1,4 @@
-;;; vulpea.el --- A collection of note-taking functions -*- lexical-binding: t; -*-
+;;; vulpea.el --- Note management library for Org mode -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2015-2025 Boris Buliga <boris@d12frosted.io>
 ;;
@@ -31,9 +31,29 @@
 ;;
 ;;; Commentary:
 ;;
-;; Vulpea is a collection of functions for note taking based on `org'.
-;; In most cases, you should simply load `vulpea' module to get all
-;; available functions.
+;; Vulpea is a note management library for Org mode that maintains its
+;; own SQLite database for efficient querying and organization.
+;;
+;; Key features:
+;; - Fast note lookup via SQLite database with automatic sync
+;; - Rich note structure: titles, aliases, tags, links, properties, metadata
+;; - Flexible querying by tags, links, properties, dates, and more
+;; - Metadata system using Org description lists
+;; - Note creation with customizable templates
+;; - Selection interface with filtering and alias expansion
+;;
+;; Quick start:
+;;   (setq vulpea-directory "~/org/")
+;;   (vulpea-db-sync-start)
+;;
+;; Main entry points:
+;; - `vulpea-find' - find and open a note
+;; - `vulpea-insert' - insert a link to a note
+;; - `vulpea-create' - create a new note
+;; - `vulpea-db-query' - query notes with a predicate
+;; - `vulpea-select' - select a note with completion
+;;
+;; See https://github.com/d12frosted/vulpea for documentation.
 ;;
 ;;; Code:
 
