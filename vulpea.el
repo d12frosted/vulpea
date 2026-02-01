5392b43459374c612d54db94bf278b98d0e73a49
Boris Buliga <boris@d12frosted.io>
2026-02-01 12:53:08 +0200

    chore: release v2.1.0

--- vulpea.el
+++ vulpea.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
-;; Version: 2.0.1
+;; Version: 2.1.0
 ;; Package-Requires: ((emacs "27.2") (org "9.4.4") (emacsql "4.3.0") (s "1.12") (dash "2.19"))
 ;;
 ;; This program is free software; you can redistribute it and/or
